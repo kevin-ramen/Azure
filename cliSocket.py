0975740da978ab5a8358861618df452d5b945aab
Kevin Ramirez Mendez <42345834+kevin-ramen@users.noreply.github.com>
2022-05-08 00:21:55 -0500

    Update cliSocket.py

--- cliSocket.py
+++ cliSocket.py
@@ -1,12 +1,14 @@
 # echo-client.py
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+HOST = "20.228.243.120"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-
-print(f"Received {data!r}")
+try:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((HOST, PORT))
+    server.sendall(b"Hello, world!")
+    data = server.recv(1024)
+    print(f"Received: {data}")
+except Exception as e:
+    print(e)
