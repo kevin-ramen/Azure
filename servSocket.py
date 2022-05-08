89195fc4c7ebb448ea3578838790980cff446642
Kevin Ramirez Mendez <42345834+kevin-ramen@users.noreply.github.com>
2022-05-07 22:31:14 -0500

    Update servSocket.py

--- servSocket.py
+++ servSocket.py
@@ -1,32 +1,15 @@
+# Echo server program
 import socket
-import sys
 
-# Creación de un socket TCP (stream) 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Como ejemplo se usará el puerto 10000
-dir_server = ('localhost', 10000)
-print('Inicializando servidor {} en el puerto {}'.format(*dir_server))
-
-sock.bind(dir_server)
-sock.listen(1)
-
-while True:
-    # Esperamos una conexión
-    print('Esperando conexión...')
-    conexion, dir_cliente = sock.accept()
-
-    try:
-        print('Conexión acpetada del cliente', dir_cliente)
-      
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
         while True:
-            data = conexion.recv(16)
-            print('Recibido {!r}'.format(data))
-            if data:
-                print('Datos recibidos...')                
-            else:
-                print('No se recibieron datos del cliente')
-                break
-    finally:
-        # Cerrar la conexión
-        conexion.close()
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
