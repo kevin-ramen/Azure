febe55846a788b80fbf9d8cf050105d5048dbf47
Kevin Ramirez Mendez <42345834+kevin-ramen@users.noreply.github.com>
2022-05-07 23:35:43 -0500

    Update cliSocket.py

--- cliSocket.py
+++ cliSocket.py
@@ -1,10 +1,12 @@
-# Echo client program
+# echo-client.py
 import socket
 
-HOST = 'daring.cwi.nl'    # The remote host
-PORT = 50007              # The same port as used by the server
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    s.sendall(b"Hello, world")
     data = s.recv(1024)
-print('Received', repr(data))
+
+print(f"Received {data!r}")
